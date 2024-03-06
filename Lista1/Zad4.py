4c69901d8ec9228cc88250d22a1957b92f62e54a
FoxFate <solembum.pospolity@gmail.com>
2024-03-07 00:50:57 +0100

    Fix Zad4

--- Lista1/Zad4.py
+++ Lista1/Zad4.py
@@ -1,7 +1,17 @@
 import numpy as np
 
-transactionMatrix = np.array([[1,1,4], [1,2,1], [1,4,6], [2,1,6], [3, 4, 1], [3,2,2]])
-itemMatrix = np.array([[1,2,0],[2,4,1], [3,6,1], [4,1,1]])
+transactionMatrix = np.array([[0,0,4], [0,1,1], [0,3,6], [1,0,6], [2,3,1], [2,1,2]])
+itemMatrix = np.array([[0,2,0],[1,4,1], [2,6,1], [3,1,1]])
+VAT = 0.23
+clientData = [dict(), dict(), dict()]
+def setUpClients():
+    clientData[0]["name"] = "Michał"
+    clientData[0]["surname"] = "Niski"
+    clientData[1]["name"] = "Kuba"
+    clientData[1]["surname"] = "Kowalski"
+    clientData[2]["name"] = "Anna"
+    clientData[2]["surname"] = "Kowalska"
+setUpClients()
 
 def checkMatrices(transactionMatrix, itemMatrix):
     print("\n", "-" * 10, "Pomyłki na paragonie", "-" * 10)
@@ -20,7 +30,7 @@ def pearsonCheck(transactionMatrix, itemMatrix):
     for transaction in transactionMatrix:
         pearson[transaction[0]] = 0
     for transaction in transactionMatrix:
-        pearson[transaction[0]] += transaction[2] * itemCost[transaction[1]]
-
-    print(pearson)
+        pearson[transaction[0]] += transaction[2] * itemCost[transaction[1]] * (1+VAT)
+    for pearsonIndex in [*pearson]:
+        print(clientData[pearsonIndex]["name"], clientData[pearsonIndex]["surname"], pearson[pearsonIndex])
 pearsonCheck(transactionMatrix, itemMatrix)
