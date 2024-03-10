4146c4fff3f154e37f9eef223bc6aef76a75f7ec
FoxFate <solembum.pospolity@gmail.com>
2024-03-10 14:56:40 +0100

    Update Zad1.py

--- Lista2/Zad1.py
+++ Lista2/Zad1.py
@@ -1,4 +1,6 @@
 # Hubert Jackowski
+import time
+
 import numpy as np
 def generateList(n):
     return [np.random.randint(0, high=9) for i in range(n)]
@@ -30,6 +32,20 @@ def selectionSort(LST):
                 result[i] = result[j]
                 result[j] = temp
     return result
+def checkExecutionTime(function, arg):
+    start = time.perf_counter_ns()
+    function(arg)
+    return time.perf_counter_ns() - start
 
-LST = generateList(10)
-print(LST, bubbleSort(LST), insertionSort(LST), selectionSort(LST))
+def avgAndMaxExecutionTime():
+    print("-"*10, "średni i maksymalny (z dziesięciu przebiegów) czas działania poszczególnych algotrytmów sortowania", "-"*10)
+    times = {"bubbleSort": [], "insertionSort": [], "selectionSort": []}
+    for i in range(10):
+        lst = generateList(1000)
+        times["bubbleSort"].append(checkExecutionTime(bubbleSort, lst))
+        times["insertionSort"].append(checkExecutionTime(insertionSort, lst))
+        times["selectionSort"].append(checkExecutionTime(selectionSort, lst))
+    print(f"bubbleSort: avg: {np.average(times["bubbleSort"])}ns max: {max(times["bubbleSort"])}ns")
+    print(f"insertionSort: avg: {np.average(times["insertionSort"])}ns max: {max(times["insertionSort"])}ns")
+    print(f"selectionSort: avg: {np.average(times["selectionSort"])}ns max: {max(times["selectionSort"])}ns")
+avgAndMaxExecutionTime()
