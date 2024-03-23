671f9606d0bd1037277817991c63068e0cbc1ecd
FoxFate <solembum.pospolity@gmail.com>
2024-03-23 15:35:22 +0100

    Finish Zad1

--- Lista4/Zad1.py
+++ Lista4/Zad1.py
@@ -1,34 +1,10 @@
 # Hubert Jackowski
-import numpy
-
-class Node:
-    def __init__(self, value=None):
-        self.value = value
-        self.children = []
+import BinaryTree
 
 
-class BinaryTree:
-    def __init__(self, root=None):
-        self.root = Node(root)
-
-    @staticmethod
-    def DFS(Node):
-        print(Node.value, end=" ")
-        for child in Node.children:
-            BinaryTree.DFS(child)
-
-
-def random():
-    return numpy.random.randint(1, 10)
-
-
-def testBFSandDFS():
-    tree = BinaryTree(random())
-    for i in range(3):
-        tree.root.children.append(Node(random()))
-        for i in range(3):
-            tree.root.children.append(Node(random()))
-            for i in range(2):
-                tree.root.children.append(Node(random()))
-    BinaryTree.DFS(tree.root)
-testBFSandDFS()
+tree = BinaryTree.formList([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+tree.print()
+print("-"*20, "DFS", "-"*20)
+tree.DFS()
+print("-"*20, "BFS", "-"*20)
+tree.BFS()
