59f9f5ecbf62dbfe8580b3df9c13e983169139fd
FoxFate <solembum.pospolity@gmail.com>
2024-03-23 22:24:17 +0100

    Add reroot method

--- Lista4/BinaryTree.py
+++ Lista4/BinaryTree.py
@@ -1,9 +1,13 @@
 # Hubert Jackowski
+import copy
+
+
 class Node:
-    def __init__(self, value=None):
+    def __init__(self, value=None, parent=None):
         self.value = value
         self.left = None
         self.right = None
+        self.parent = parent
 
 
 class BinaryTree:
@@ -50,30 +54,70 @@ class BinaryTree:
 
         print()
 
+    def getLeaf(self, value: int) -> Node or None:
+        targetNode = None
 
-def formList(lst) -> BinaryTree:
-    def _buildTree(values, index) -> Node:
-        currentRoot = Node(values[index])
-        if 2 * index + 1 < len(values):
-            currentRoot.left = _buildTree(values, 2 * index + 1)
-        if 2 * index + 2 < len(values):
-            currentRoot.right = _buildTree(values, 2 * index + 2)
+        def _DFS(node: Node):
+            nonlocal targetNode
+            if node.value == value:
+                targetNode = node
+            if node.left is not None:
+                _DFS(node.left)
+            if node.right is not None:
+                _DFS(node.right)
 
-        return currentRoot
+        _DFS(self.root)
+        return targetNode
 
-    def _cutBlankBranches(node: Node):
-        if node.left is not None:
-            if node.left.value is None:
-                node.left = None
-            else:
-                _cutBlankBranches(node.left)
+    def reroot(self, leaf: Node):
+        if leaf.right is not None and leaf.left is not None:
+            raise Exception("Too Many Children")
 
-        if node.right is not None:
-            if node.right.value is None:
-                node.right = None
-            else:
-                _cutBlankBranches(node.right)
+        currentNode = leaf
+        parent = currentNode.parent
+        while currentNode != self.root:
+            grandParent = parent.parent
 
-    treeRoot = _buildTree(lst, 0)
-    _cutBlankBranches(treeRoot)
-    return BinaryTree(treeRoot)
+            if currentNode.left:
+                currentNode.right = currentNode.left
+
+            currentNode.left = parent
+            parent.parent = currentNode
+            if parent.left == currentNode:
+                parent.left = None
+            elif parent.right == currentNode:
+                parent.right = None
+
+            currentNode = parent
+            parent = grandParent
+
+        leaf.parent = None
+        self.root = leaf
+
+    @staticmethod
+    def formList(lst: list[int]):
+        def _buildTree(values, index=0, parent=None) -> Node:
+            currentRoot = Node(values[index], parent)
+            if 2 * index + 1 < len(values):
+                currentRoot.left = _buildTree(values, 2 * index + 1, currentRoot)
+            if 2 * index + 2 < len(values):
+                currentRoot.right = _buildTree(values, 2 * index + 2, currentRoot)
+
+            return currentRoot
+
+        def _cutBlankBranches(node: Node):
+            if node.left is not None:
+                if node.left.value is None:
+                    node.left = None
+                else:
+                    _cutBlankBranches(node.left)
+
+            if node.right is not None:
+                if node.right.value is None:
+                    node.right = None
+                else:
+                    _cutBlankBranches(node.right)
+
+        treeRoot = _buildTree(lst)
+        _cutBlankBranches(treeRoot)
+        return BinaryTree(treeRoot)
