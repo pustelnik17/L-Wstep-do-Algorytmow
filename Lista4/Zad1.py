51fc9e484c171b3790d15742fffe79e0243254d3
FoxFate <solembum.pospolity@gmail.com>
2024-03-23 22:24:06 +0100

    Add Zad 2

--- Lista4/Zad1.py
+++ Lista4/Zad1.py
@@ -2,7 +2,7 @@
 import BinaryTree
 
 
-tree = BinaryTree.formList([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+tree = BinaryTree.BinaryTree.formList([_ for _ in range(31)])
 tree.print()
 print("-"*20, "DFS", "-"*20)
 tree.DFS()
--- Lista4/Zad2.py
+++ Lista4/Zad2.py
@@ -1,1 +1,8 @@
 # Hubert Jackowski
+import BinaryTree
+
+
+tree = BinaryTree.BinaryTree.formList([_ for _ in range(31)])
+tree.print()
+tree.reroot(tree.getLeaf(30))
+tree.print()
