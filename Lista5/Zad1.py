dbee851e0d5e19daa9c60434287dde327ea9ff79
FoxFate <solembum.pospolity@gmail.com>
2024-03-25 00:25:15 +0100

    Add Zad1

---
+++ Lista5/Zad1.py
@@ -0,0 +1,31 @@
+# Hubert Jackowski
+from TextCompare import TextCompare
+
+print("-"*20, "Hamming Distance", "-"*20)
+left, right = "karolin", "kathrin"
+print(left, right, TextCompare.hammingDistance(left, right))
+left, right = "karolin", "kerstin"
+print(left, right, TextCompare.hammingDistance(left, right))
+left, right = "kathrin", "kerstin"
+print(left, right, TextCompare.hammingDistance(left, right))
+left, right = "0000", "1111"
+print(left, right, TextCompare.hammingDistance(left, right))
+left, right = "2173896", "2233796"
+print(left, right, TextCompare.hammingDistance(left, right))
+
+print("-"*20, "Hamming Distance Keyboard Slip", "-"*20)
+left, right = "kathrin", "kerstin"
+print(left, right, TextCompare.hammingDistanceKeyboardSlip(left, right))
+left, right = "wer", "wee"
+print(left, right, TextCompare.hammingDistanceKeyboardSlip(left, right))
+left, right = "wer", "wer"
+print(left, right, TextCompare.hammingDistanceKeyboardSlip(left, right))
+left, right = "wer", "wey"
+print(left, right, TextCompare.hammingDistanceKeyboardSlip(left, right))
+
+
+print("-"*20, "Hamming Distance Keyboard Slip", "-"*20)
+print(TextCompare.dictionarySimilar("reel"))
+print(TextCompare.dictionarySimilar("reels"))
+print(TextCompare.dictionarySimilar("ree"))
+print(TextCompare.dictionarySimilar("rf"))
